f9d50a68954a440632302c2e643881d060ed192f
Pablo Salvi <salvipablo@gmail.com>
2022-01-10 14:07:40 -0300

    Toma de datos en educacion por JSON
    Resolve: R#001

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataDbService } from 'src/app/services/data-db.service';
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  dataPortfolio: any;
 
-  constructor() { }
+  constructor(private dataPort: DataDbService) { }
 
   ngOnInit(): void {
+    this.dataPort.obtainData().subscribe(data => {
+      this.dataPortfolio = data.education;
+      console.log(this.dataPortfolio);
+    })
   }
 
 }
